fcf9d6f4c8233ead4003346371874695f3ab6ae3
Zury44 <u20211198103@usco.edu.co>
2025-09-09 15:31:16 -0500

    cambio carpeta

---
+++ FrontendMovil/app/modulos/inventories.jsx
@@ -0,0 +1,12 @@
+import camara from "../camera/camera";
+
+const OtraPantalla = () => {
+  return (
+    <div>
+      <h2>Esta es otra pantalla</h2>
+      <camara />
+    </div>
+  );
+};
+
+export default camara;
